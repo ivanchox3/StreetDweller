4fd4eb7da6985a7a80678a980d28b4980023bf79
Ivan Masapanta <ivan_mag@hotmail.com>
2018-11-05 18:35:31 -0500

    Actualizacion iconos, splash y pagina registro habitantes

---
+++ src/pages/reghabitantes/reghabitantes.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { ReghabitantesPage } from './reghabitantes';
+
+@NgModule({
+  declarations: [
+    ReghabitantesPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(ReghabitantesPage),
+  ],
+})
+export class ReghabitantesPageModule {}
